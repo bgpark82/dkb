35a4d1b19bf3ab73d1c8752be43427b3196ec0d2
bgpark82 <bgpark82@gmail.com>
2025-03-16 23:23:29 +0100

    init

---
+++ src/main/kotlin/com/bgpark/demo/dkb/DkbApplication.kt
@@ -0,0 +1,11 @@
+package com.bgpark.demo.dkb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DkbApplication
+
+fun main(args: Array<String>) {
+    runApplication<DkbApplication>(*args)
+}
---
+++ src/test/kotlin/com/bgpark/demo/dkb/DkbApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bgpark.demo.dkb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DkbApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
